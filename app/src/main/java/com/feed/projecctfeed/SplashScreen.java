38d29b0f3fb31c62b7e81787b4a7bdd6927a6d81
Prashant Gupta <pkgeffects@gmail.com>
2018-09-14 11:28:11 +0530

    Initial commit

---
+++ app/src/main/java/com/feed/projecctfeed/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.feed.projecctfeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // TODO
+    }
+}
